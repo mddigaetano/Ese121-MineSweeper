5ab8e27286548691a890a4b70ad1dc3c3af7f734
lcmiccioni <miccioni.luca@gmail.com>
2016-05-25 10:51:19 +0200

    Implementazione finestra in corso

--- src/MineSweeper/Finestra.java
+++ src/MineSweeper/Finestra.java
@@ -17,6 +17,7 @@
 package MineSweeper;
 
 import java.awt.*;
+import java.util.Random;
 import javax.swing.*;
 
 /**
@@ -27,32 +28,32 @@ public class Finestra extends JFrame {
 
     Casella[][] buttons;
     JPanel panel;
+    int nMine;
 
     public Finestra(int dim, int prob) {
-        //Variabile temporanea per la memorizzazione del numero delle mine generate
-        int nMine = 0;
         this.setLayout(new BorderLayout());
         panel = new JPanel(new GridLayout(dim, dim));
         buttons = new Casella[dim][dim];
+        this.creaMatriceFittizia(dim, prob);
         //Popolamento della matrice di buttoni
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                //Generazione numero random da 1 a 100 
-                //E confronto con la probabilità inserita dall'utente                
-                if (Math.random() * (100 - 1 + 1) <= prob) {
-                    //Se il numero generato è minore del valore prob (probabilità inserita dall'utente)
-                    //Impostiamo tramite il costruttore di Casella il suo valore a -1 (bomba)
-                    panel.add(buttons[i][j] = new Casella(i, j, -1));
-                    buttons[i][j].setText("riga" + i + "-----" + "colonna" + j + "-------" + buttons[i][j].getValue());
-                    //Incremento il numero di mine generate
-                    nMine++;
-                } else //Altrimenti impostiamo tramite il costruttore di Casella il suo valore a 0 (non bomba)
-                {
-                    panel.add(buttons[i][j] = new Casella(i, j, 0));
-                }
-                buttons[i][j].setText("riga" + i + "-----" + "colonna" + j + "--------" + buttons[i][j].getValue());
-            }
-        }
+//        for (int i = 0; i < dim; i++) {
+//            for (int j = 0; j < dim; j++) {
+//                //Generazione numero random da 1 a 100 
+//                //E confronto con la probabilità inserita dall'utente                
+//                if (Math.random() * (100 - 1 + 1) <= prob) {
+//                    //Se il numero generato è minore del valore prob (probabilità inserita dall'utente)
+//                    //Impostiamo tramite il costruttore di Casella il suo valore a -1 (bomba)
+//                    panel.add(buttons[i][j] = new Casella(i, j, -1));
+//                    buttons[i][j].setText("riga" + i + "-----" + "colonna" + j + "-------" + buttons[i][j].getValue());
+//                    //Incremento il numero di mine generate
+//                    nMine++;
+//                } else //Altrimenti impostiamo tramite il costruttore di Casella il suo valore a 0 (non bomba)
+//                {
+//                    panel.add(buttons[i][j] = new Casella(i, j, 0));
+//                }
+//                buttons[i][j].setText("riga" + i + "-----" + "colonna" + j + "--------" + buttons[i][j].getValue());
+//            }
+//        }
 
 //            Timer t = new Timer(nMine);
 //            Thread t1 = new Thread(t);
@@ -60,6 +61,34 @@ public class Finestra extends JFrame {
 //            this.add(t,"North");
         this.add(panel, "Center");
 
+    }
+    
+    private int[][] creaMatriceFittizia(int DIM, int prob){
+        
+        int matrix[][] = new int[DIM+2][DIM+2];
+        Random rnd = new Random();
+        
+        for (int i = 1; i <= DIM; i++) {
+            for (int j = 1; j <= DIM; j++) {
+                //Generazione numero random da 1 a 100 
+                //E confronto con la probabilità inserita dall'utente                
+                if (rnd.nextInt(100)+1 <= prob) {
+                    //Se il numero generato è minore del valore prob (probabilità inserita dall'utente)
+                    //Impostiamo il suo valore a -1 (bomba)
+                    matrix[i][j] = -1;
+                    //Incremento il numero di mine generate
+                    nMine++;
+                    
+                    if()// TODO evitare mina
+                        //Addizionamo 1 alle caselle vicine, in senso orario
+                        matrix[i-1][j-1] += 1;
+                        matrix[i-1][j] += 1;
+                        matrix[i-1][j+1] += 1;
+                } 
+            }
+        }
+        
+        return matrix;
     }
 
 }
